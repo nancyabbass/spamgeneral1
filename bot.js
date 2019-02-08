d7f3fe5ecb53b4dbdd8553dfdeb0816214e12063
nancyabbass <47457423+nancyabbass@users.noreply.github.com>
2019-02-08 19:09:19 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("543467202364440586")
+setInterval(function() {
+channel.send(`im nanct the crazy lol`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
